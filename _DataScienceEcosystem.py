1269786bdff3e8623ca1e5a39b542c8a9241b658
abdelouahab-e <139826565+abdelouahab-e@users.noreply.github.com>
2023-07-19 13:45:52 +0100

    Data Science Tools and Ecosystem

---
+++ _DataScienceEcosystem.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# __Objectives:__
+# . python
+# . julia
+# . javascript
+# . SQL
+
+# Some of the popular languages that Data Scientists use are:
+# 1. TensorFlow.
+# 2. NumPy.
+# 3. SciPy.
+
+# |Data Science Tools|
+# |------------------|
+# |apache spark|
+# |tensor flow|
+# |sas|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[12]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[14]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author
+# abdelouahab
